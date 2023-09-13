ba2a2c2428ea2f588a43b1a3055dd195afd3f361
Matthieu Falce <matthieu@falce.net>
2023-09-13 09:25:22 +0200

    gitautosync update 54

---
+++ .history/tape_en_cours/jour2/commentaires_docstring_20230913092515.py
@@ -0,0 +1,19 @@
+# un commentaire 
+
+def ma_fonction():
+    """La doc de ma fonction."""
+    pass 
+
+def ma_fonction_avec_arguments(a:int, b: list) -> str: 
+    """Une super fonction
+
+    Args:
+        a (int): un paramètre
+        b (list): un autre
+
+    Returns:
+        str: le retour
+    """
+
+ma_fonction()
+list((1, 2, 3))
