92e5d82dc5b6b50bdef51f6f54f41c8c244be42e
Matthieu Falce <matthieu@falce.net>
2023-09-13 11:27:12 +0200

    gitautosync update 119

---
+++ .history/tape_en_cours/jour2/my_map_20230913112705.py
@@ -0,0 +1,21 @@
+def my_map(f, iterator, *autre_nombres):
+    res = []
+    for values in zip(iterator, *autre_nombres):
+        values = list(values)
+        f_result = f(*values)
+        res.append(f_result)
+    return res 
+
+def multiplication(a, b, c):
+    return a * b * c
+
+
+print(my_map(multiplication, 
+       [1, 2, 3], 
+       [4, 5, 6], 
+       [7, 8, 9], 
+))
+
+# multiplication(1, 4, 7)
+# multiplication(2, 5, 8)
+# multiplication(3, 6, 9)
