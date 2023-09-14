d0107f7a560b4bee63f4f93c96ad3bf2628a884f
Matthieu Falce <matthieu@falce.net>
2023-09-14 14:49:07 +0200

    gitautosync update 355

---
+++ .history/tape_en_cours/jour3/test_fizzbuzz_20230914144906.py
@@ -0,0 +1,17 @@
+import fizz_buzz_retours_Nghiem
+
+def test_fizz_buzz_3():
+    res = fizz_buzz_retours_Nghiem.handle_number(3)
+    assert res == "3 fizz"
+    
+def test_fizz_buzz_5():
+    res = fizz_buzz_retours_Nghiem.handle_number(5)
+    assert res == "5 buzz"
+
+# def test_fizz_buzz_15():
+#     res = fizz_buzz_retours_Nghiem.handle_number(15)
+#     assert res == "15 fizzbuzz"
+
+# def test_fizz_buzz_autre():
+#     res = fizz_buzz_retours_Nghiem.handle_number(4)
+#     assert res == "4"
