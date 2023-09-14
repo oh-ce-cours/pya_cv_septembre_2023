09671ac7690d847a3cace5e2d6b1fc88ecc9152e
Matthieu Falce <matthieu@falce.net>
2023-09-14 11:58:28 +0200

    gitautosync update 281

---
+++ .history/tape_en_cours/jour3/demo_nonlocal_20230914115818.py
@@ -0,0 +1,14 @@
+cache = 1 
+
+def outer():
+    cache = 2
+    def inner():
+        nonlocal cache
+        print(cache) 
+        cache = 3
+        print(cache)
+    print(cache)
+    inner()
+    print(cache)
+
+outer()
