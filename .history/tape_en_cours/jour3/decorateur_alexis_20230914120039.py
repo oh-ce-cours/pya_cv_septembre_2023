502118081b608f5d029c8bc9ac4067910b37a980
Matthieu Falce <matthieu@falce.net>
2023-09-14 12:00:42 +0200

    gitautosync update 283

---
+++ .history/tape_en_cours/jour3/decorateur_alexis_20230914120039.py
@@ -0,0 +1,42 @@
+# Décorateur pour les ingrédients
+def ingredient_haut(param):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            print(f"{param}") 
+            sortie = f(*args, **kwargs)
+            return sortie
+        return wrapper
+    return decorator
+
+
+def ingredient_bas(param):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            sortie = f(*args, **kwargs)
+            print(f"{param}") 
+            return sortie
+        return wrapper
+    return decorator
+
+# Décorateur pour le pain
+def pain(f):
+    def wrapper(*args, **kwargs):
+        print("Pain")
+        result = f(*args, **kwargs)
+        print("Pain")
+        return result
+    return wrapper
+
+# Fonction pour créer un sandwich
+@pain
+@ingredient_haut("salade")
+@ingredient_haut("tomate")
+@ingredient_bas("salade")
+@ingredient_bas("fromage")
+def sandwich(viande):
+    print(viande)
+
+sandwich = ingredient_bas("salade")(ingredient_bas("fromage")(sandwich))
+
+# Créez un sandwich en utilisant le décorateur
+sandwich("steak")
